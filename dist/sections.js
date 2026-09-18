8c1854d015247a4be4ad751be61f824d4249c00d
rucrazia <rucrazia@gmail.com>
2026-09-19 00:35:07 +0900

    Increase contrast of brain planes and intersection outlines

--- dist/sections.js
+++ dist/sections.js
@@ -1,5 +1,5 @@
 import * as T from './vendor/three.module.js';
-import {sectionSegments} from './planes.js?v=20';
+import {sectionSegments} from './planes.js?v=21';
 
 // Join mesh/plane intersections into closed rings, preserving nested cavities.
 export function sectionGeometry(meshes,center,quaternion){
